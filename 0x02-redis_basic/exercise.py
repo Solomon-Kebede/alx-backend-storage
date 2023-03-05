1d5ddf9273130f0a79a16988d22686ae7c52f10e
Solomon Kebede <solomonaau188@gmail.com>
2023-03-05 16:44:24 +0000

    Task 1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-0. Writing strings to Redis
+1. Reading from Redis and recovering original type
 """
 
 import redis
@@ -19,3 +19,19 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key, fn):
+        """Get values in redis"""
+        value = self._redis.get(key)
+        if fn is not None:
+            return fn(value)
+        elif fn is None:
+            return value
+
+    def get_str(*cache):
+        """Convert to string"""
+        return str(cache)
+
+    def get_int(*cache):
+        """Convert to integer"""
+        return int(cache)
