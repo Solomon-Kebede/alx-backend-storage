289eab83f71705b3f15a6674f08095822afeb9c6
Solomon Kebede <solomonaau188@gmail.com>
2023-03-26 18:08:39 +0000

    Task 0 - table name fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -10,7 +10,7 @@
 -- your business rules and avoid bugs in your application
 
 
-CREATE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
