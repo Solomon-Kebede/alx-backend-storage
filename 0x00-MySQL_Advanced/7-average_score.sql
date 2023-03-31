510430de3eeadd248acd42c3cca12e00f01700c9
Solomon Kebede <solomonaau188@gmail.com>
2023-03-31 18:37:49 +0000

    Task 7. Average score(Y)(N)(Y)

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,12 +3,23 @@
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 
+-- DELIMITER //
+
+-- CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+-- BEGIN
+--   SELECT AVG(score) AS average_score FROM corrections WHERE corrections.user_id = user_id;
+--   UPDATE users SET average_score = average_score WHERE id = user_id;
+-- END //
+
+-- DELIMITER ;
+
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  SELECT AVG(score) AS average_score FROM corrections WHERE corrections.user_id = user_id;
-  UPDATE users SET average_score = average_score WHERE id = user_id;
+  DECLARE avg_score DECIMAL(10, 2);
+  SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+  UPDATE users SET average_score = avg_score WHERE id = user_id;
 END //
 
 DELIMITER ;
