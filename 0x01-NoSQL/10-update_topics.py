38da7fd91c46d60bfc0ba0d541a06085797327d2
Solomon Kebede <solomonaau188@gmail.com>
2023-03-31 19:34:38 +0000

    Task 10 - return statement

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -14,4 +14,4 @@ document based on the name:
 
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    return mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
