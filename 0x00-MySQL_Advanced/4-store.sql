8f8aaae2bf7ef777ea28a45b4e5df3068a235e93
Solomon Kebede <solomonaau188@gmail.com>
2023-03-31 12:51:07 +0000

    Task 4. Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+-- Quantity in the table `items` can be negative.
+
+-- **Context:** *Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!*
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items SET quantity - NEW.number WHERE name = NEW.item_name;
+END;
