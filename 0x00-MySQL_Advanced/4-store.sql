a476c9bc209b961d818f69a3afaef1bee1c96ef5
Solomon Kebede <solomonaau188@gmail.com>
2023-03-31 13:56:42 +0000

    Task 4. Buy buy buy (Y)(N)

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,9 +5,9 @@
 -- **Context:** *Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!*
 
 CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON orders
+AFTER INSERT ON `orders`
 FOR EACH ROW
 BEGIN
     -- UPDATE SET items.quantity = items.quantity - orders.number;
-    UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+    UPDATE `items` SET `quantity` = `quantity` - NEW.number WHERE name = NEW.item_name;
 END;
