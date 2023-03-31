22793d0420ec2a4bae92ce652211a2e16d61d865
Solomon Kebede <solomonaau188@gmail.com>
2023-03-31 14:43:50 +0000

    Task 5. Email validation to sent (N)

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,27 @@
+-- Write a SQL script that creates a trigger that resets the attribute `valid_email` only when the `email` has been changed.
+
+-- **Context**: *`Nothing related to MySQL, but perfect for user email validation - distribute the logic to the database itself!*
+
+-- DELIMETER //
+-- CREATE TRIGGER reset_valid_email
+-- BEFORE UPDATE ON `user`
+-- FOR EACH ROW 
+-- BEGIN
+--   IF NEW.email != OLD.email THEN
+--     SET NEW.valid_email = false;
+--   END IF;
+-- END //
+-- DELIMETER ;
+
+DELIMITER //
+
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON `users` FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+
+END //
+
+DELIMITER ;
